626ebcdfc245e7a5a5b88a4211c30d9a308f179d
Peter Khalil <uni.03c9@gmail.com>
2022-02-14 20:03:18 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
-        </Routes>
-        {/* <HomePage />
-      <Outlet /> */}
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
