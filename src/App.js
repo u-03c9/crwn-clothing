2544a31ea2e20f851a4ffda145dded87023491e2
Peter Khalil <uni.03c9@gmail.com>
2022-02-14 21:02:02 +0200

    basic sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/login" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
